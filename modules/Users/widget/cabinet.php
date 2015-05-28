af496855ca3cf939fc14e925ef8402f13d9d09d6
babagay <wonderer@i.ua>
2015-05-28 14:51:39 +0300

    Мои заказы ссылка

--- modules/Users/widget/cabinet.php
+++ modules/Users/widget/cabinet.php
@@ -23,38 +23,38 @@ return
           $products = $db->fetchAll($query);
          */
 
-        $view = app()->getView();
+        $view       = app()->getView();
         $app_object = app()->getInstance();
-        $db =  app()->getDb();
-        $basket = app()->getSession()->basket;
-        $user = app()->getAuth()->getIdentity();
+        $db         = app()->getDb();
+        $basket     = app()->getSession()->basket;
+        $user       = app()->getAuth()->getIdentity();
         //$params = $app_object->getRequest()->getParams();
 
         $header_enter_cabinet = "Войти на сайт";
-        $header_basket = "Ваша корзина";
-        $link_basket = $view->baseUrl("корзина");
-        $link_signup = $view->baseUrl("регистрация");
-        $header_signup = "Зарегистрироваться";
+        $header_basket        = "Ваша корзина";
+        $link_basket          = $view->baseUrl("корзина");
+        $link_signup          = $view->baseUrl("регистрация");
+        $header_signup        = "Зарегистрироваться";
 
-        $discount = 0;
-        $credit = 0;
+        $discount        = 0;
+        $credit          = 0;
         $orders_to_bonus = 10;
-        $in_basket = '';
+        $in_basket       = '';
 
-        if(is_object($user)){
-            $userData = $db->fetchObject("SELECT * FROM users WHERE id = ". $db->quote($user->id));
+        if (is_object($user)) {
+            $userData = $db->fetchObject("SELECT * FROM users WHERE id = " . $db->quote($user->id));
 
-            $credit = round($userData->credit * 100,0)/100; // Отсекает 3 и 4 знаки после запятой, не затрагивая значение в базе
-            $discount = $userData->discount;
+            $credit          = round($userData->credit * 100, 0) / 100; // Отсекает 3 и 4 знаки после запятой, не затрагивая значение в базе
+            $discount        = $userData->discount;
             $orders_to_bonus = $userData->orders_to_bonus;
         }
 
-        if(isset($basket['products'])){
-            if(sizeof($basket['products'])){
+        if (isset($basket['products'])) {
+            if (sizeof($basket['products'])) {
                 $tmp = array();
-                foreach($basket['products'] as $products_id => $products_num){
-                    if($products_num == 0){
-                        app()->dispatch("Basket","ajax/modify_basket",['products_id'=>$products_id,'mode'=>'remove']);
+                foreach ($basket['products'] as $products_id => $products_num) {
+                    if ($products_num == 0) {
+                        app()->dispatch("Basket", "ajax/modify_basket", ['products_id' => $products_id, 'mode' => 'remove']);
                     } else {
                         $tmp[$products_id] = $products_num;
                     }
@@ -63,19 +63,19 @@ return
             }
             $products_count = count($basket['products']);
 
-            $product_title = app()->wordEnd("товар",$products_count);
+            $product_title = app()->wordEnd("товар", $products_count);
 
-            $in_basket = " - <span id=\"basket_products_counter\">".$products_count . "</span> $product_title";
+            $in_basket = " - <span id=\"basket_products_counter\">" . $products_count . "</span> $product_title";
         }
 
         $btn_1 = "ВЫХОД";
 
         $link_enter_cabinet = $view->baseUrl("вход");
-        $link_btn_1 = '';
+        $link_btn_1         = '';
 
-        if ($identity = app()->getAuth()->getIdentity()){
+        if ($identity = app()->getAuth()->getIdentity()) {
             $header_enter_cabinet = "Здравствуйте, " . $identity->login . "!"; // TODO сделать линк Войти в кабинет
-            $link_enter_cabinet = $link_btn_1 = $view->baseUrl("выход");
+            $link_enter_cabinet   = $link_btn_1 = $view->baseUrl("выход");
         }
 
         //$user = Users\Table::findRow(20);
@@ -89,8 +89,9 @@ return
             require(['bluz.widget']);
         </script>
 
-        <div class="cabinet"><a href="<?php echo $link_enter_cabinet; ?>" ><?php echo $header_enter_cabinet; ?></a></div>
-        <div class="basket"><a href="<?php echo $link_basket; ?>" > <?php echo $header_basket; ?> <?php echo $in_basket; ?> </a></div>
+        <div class="cabinet"><a href="<?php echo $link_enter_cabinet; ?>"><?php echo $header_enter_cabinet; ?></a></div>
+        <div class="basket"><a
+                href="<?php echo $link_basket; ?>"> <?php echo $header_basket; ?> <?php echo $in_basket; ?> </a></div>
         <div class="partners">
             <img src="public/images/bank.png">
             <img src="public/images/master.png" class="middle">
@@ -98,8 +99,8 @@ return
         </div>
         <?php
 
-        if( !is_null($identity) ){
-        ?>
+        if (!is_null($identity)) {
+            ?>
             <div class="cabnet-form">
                 <div class="select-cabnet"><a href="кабинет" id="cabinet_header">Личный
                         кабинет: </a>&#9660;</span>  </span>
@@ -107,23 +108,26 @@ return
                         <!--li><a href="">Система СКИДОК</a></li-->
                         <!--li><a href="">Пополнение счета</a></li-->
                         <li><a href="кабинет/мой_профиль">Мой профиль</a></li>
+                        <li><a href="кабинет/мои_заказы">Мои заказы</a></li>
                     </ul>
                 </div>
                 <p>Текущая скидка: <span class="discount"><?php echo $discount; ?> %</span></p>
+
                 <p>Баланс: <span class="discount" id="users_credit"><?php echo $credit; ?></span> грн.</p>
-                <p>Заказов до ПОДАРКА: <span class="discount"><?php echo $orders_to_bonus; ?></span>  </p>
+
+                <p>Заказов до ПОДАРКА: <span class="discount"><?php echo $orders_to_bonus; ?></span></p>
                 <!--button-->
-                  <a href="<?php echo $link_enter_cabinet; ?>" ><?php echo $btn_1; ?></a>
+                <a href="<?php echo $link_enter_cabinet; ?>"><?php echo $btn_1; ?></a>
                 <!--/button-->
             </div>
         <?php
         } else {
             ?>
-            <div class="cabinet"><a href="<?php echo $link_signup; ?>" ><?php echo $header_signup; ?></a></div>
-            <?php
+            <div class="cabinet"><a href="<?php echo $link_signup; ?>"><?php echo $header_signup; ?></a></div>
+        <?php
         }
 
-    return false; // Чтобы не выкидывало ошибку Unable to find template "widget.phtml" при вызове через аякс
+        return false; // Чтобы не выкидывало ошибку Unable to find template "widget.phtml" при вызове через аякс
 
 
     };
