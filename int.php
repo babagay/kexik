7f550714c84fb5e39cf80d08418d42b7b61503ac
babagay <wonderer@i.ua>
2015-07-01 16:11:25 +0300

    test mail

--- int.php
+++ int.php
@@ -1,5 +1,18 @@
 <?php
 
+    $to      = 'wonderer@i.ua';
+    $subject = 'the subject';
+    $message = 'hello';
+    $headers = 'From: webmaster@example.com' . "\r\n" .
+        'Reply-To: webmaster@example.com' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    mail($to, $subject, $message, $headers);
+
+    echo 1;
+
+
+
 echo "[root test]+ <br/>";
 // Show all information, defaults to INFO_ALL
 phpinfo();
