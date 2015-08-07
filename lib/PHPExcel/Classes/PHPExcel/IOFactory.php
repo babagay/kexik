598861905baac6afa87935e52b7492fff46f332d
babagay <wonderer@i.ua>
2015-08-07 18:21:10 +0300

    тестовый коммит

--- lib/PHPExcel/Classes/PHPExcel/IOFactory.php
+++ lib/PHPExcel/Classes/PHPExcel/IOFactory.php
@@ -188,7 +188,7 @@ class PHPExcel_IOFactory
 	 * @throws	PHPExcel_Reader_Exception
 	 */
 	public static function load($pFilename) {
-		$reader = self::createReaderForFile($pFilename);
+		$reader = self::createReaderForFile($pFilename);fb(555);
 		return $reader->load($pFilename);
 	}	//	function load()
 
