ebdc60272992d4a469981fb51c4232633f7a7e82
babagay <wonderer@i.ua>
2016-02-24 18:26:06 +0200

    clean file

--- modules/Admin/ajax/operations.php
+++ modules/Admin/ajax/operations.php
@@ -1,72 +1,64 @@
 <?php
+/**
+ * AJAX closure
+ *
+ * @author   Anton Shevchuk
+ * @created  26.09.11 17:41
+ * @return closure
+ *
+ *
+ * Для случая , когда ответ ожидается в JSON
+ * return function() {
+ *     $res = array(
+ *     'response' => 'ok',
+ *     );
+ *     return $res;
+ *     };
+ */
+namespace Application;
+
+use Bluz;
+
+/**
+ * @var \Application\Bootstrap $_this
+ */
+$_this = $this;
+
+return
+
     /**
-     * AJAX closure
-     *
-     * @author   Anton Shevchuk
-     * @created  26.09.11 17:41
-     * @return closure
+     * @var \Bluz\View\View $view
      */
-    namespace Application;
+    function ($articles_id = null) use ($view, $_this) {
 
-    use Bluz;
+        $db = app()->getDb();
 
-    $_this = $this;
+        $mess = $title = '';
 
-    return
+        $operation = app()->getInstance()->getRequest()->operation;
 
-        function ($articles_id = null ) use ($view,$_this) {
-            /**
-             * @var \Application\Bootstrap $this
+        switch ($operation) {
+            case 'hide':
+                $visibility = 0;
+                $db->query("UPDATE zoqa_articles SET visibility = :visibility WHERE articles_id = :articles_id", array( 'visibility' => $visibility, 'articles_id' => $articles_id ));
+                $title = "Операция: [$operation]";
+                $mess = "Статья скрыта";
+                break;
+            case 'unhide':
+                $visibility = 1;
+                $db->query("UPDATE zoqa_articles SET visibility = :visibility WHERE articles_id = :articles_id", array( 'visibility' => $visibility, 'articles_id' => $articles_id ));
+                $title = "Операция: [$operation]";
+                $mess = "Статья открыта";
+                break;
+            default:
+                break;
+        }
 
-            fb($view); //  Bluz\View\View
-            fb($_this); //  Application\Bootstrap
-             *
-             * @var \Bluz\View\View $view
-             */
-
-            $db =  app()->getDb();
-
-            /*
-             * // Для случая , когда ответ ожидается в JSON
-            return function() {
-
-                $res = array(
-                    'response' => 'ok',
-                );
-
-                return $res;
-            };
-            */
-
-            $mess = $title = '';
-
-           $operation =  app()->getInstance()->getRequest()->operation;
-
-
-            switch($operation){
-                case 'hide':
-                    $visibility = 0;
-                    $db->query("UPDATE zoqa_articles SET visibility = :visibility WHERE articles_id = :articles_id", array('visibility' => $visibility, 'articles_id' => $articles_id));
-                    $title = "Операция: [$operation]";
-                    $mess = "Статья скрыта";
-                    break;
-                case 'unhide':
-                    $visibility = 1;
-                    $db->query("UPDATE zoqa_articles SET visibility = :visibility WHERE articles_id = :articles_id", array('visibility' => $visibility, 'articles_id' => $articles_id));
-                    $title = "Операция: [$operation]";
-                    $mess = "Статья открыта";
-                    break;
-                default:
-                    break;
-            }
-
-
-
-            return function () use($mess, $title) {
-                echo "<div class='jumbotron'><div class='container'>";
-                echo "<h3>$title</h3>";
-                echo "<p class='text-warning text-primary'>$mess</p>";
-                echo "</div></div>";
-            };
-
+        return function () use ($mess, $title) {
+            echo "<div class='jumbotron'><div class='container'>";
+            echo "<h3>$title</h3>";
+            echo "<p class='text-warning text-primary'>$mess</p>";
+            echo "</div></div>";
         };
+
+    };
